3abecc4b367d15b78a967d07a521d1558e0e2042
Prasiinus <valentind9@hotmail.fr>
2023-03-17 18:56:53 +0100

    curseur cross

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Etch-a-Sketch
 Browser version of something between a sketchpad and an Etch-A-Sketch.
+
+
+Modifier le range avec une liste 
+
+faire un remove child si on appue à nouveau sur range 
--- script.js
+++ script.js
@@ -1,31 +1,48 @@
 const Container = document.querySelector('.container');
+const range = document.querySelector('.range');
+const inputvalue = document.querySelector('.inputvalue')
 
-let size=16;
+const validsize = [8, 16, 32, 64];
+const defaultvalue = validsize[range.value];
 
-for (let i = 0; i < size; i++) {
-    const raw = document.createElement('div');
-    raw.setAttribute('class','raw');
-    Container.appendChild(raw);
-    
-    for (let i = 0; i < size; i++) {
-        const div = document.createElement('div');
-        div.setAttribute('class','pix')
-        raw.appendChild(div);
-    //     div.addEventListener("click", () => {
-    //     div.addEventListener("mouseenter", () => {
-    //         div.style.backgroundColor = 'red';
-    //     });
-    // })
+board(defaultvalue);
+inputvalue.textContent = defaultvalue + 'x' + defaultvalue;
 
+range.addEventListener("click", function (e) {
+    let size = validsize[this.value];
+    inputvalue.textContent = `${size}x${size}`;;
+    board(size);
+});
+
+
+function board(size) {
+
+    while (Container.firstChild) {
+        Container.removeChild(Container.firstChild);
     }
-}
-const boxes = [...document.getElementsByClassName('pix')];
-        const makeRed = event => event.target.style.backgroundColor = 'red';
-        
-        function color(box) {
-          box.addEventListener("mousedown", makeRed);
-          box.addEventListener("mouseover", event => {
-            if (event.buttons == 1) makeRed(event);
-          });
+
+    for (let i = 0; i < size; i++) {
+        const raw = document.createElement('div');
+        raw.setAttribute('class', 'raw');
+        Container.appendChild(raw);
+        for (let i = 0; i < size; i++) {
+            const pixel = document.createElement('div');
+            pixel.setAttribute('class', 'pix')
+            raw.appendChild(pixel);
+            color(pixel)
         }
-boxes.forEach(color);
+    }
+
+}
+
+function color(pixel) {
+    pixel.addEventListener("mousedown", makeRed);
+    pixel.addEventListener("mouseover", event => {
+        if (event.buttons == 1) makeRed(event);
+    });
+}
+
+function makeRed(event) {
+    event.target.style.backgroundColor = 'red';
+}
+
