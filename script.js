438fb799672af5e36a869b5d0c4b821017e13a08
Prasiinus <valentind9@hotmail.fr>
2023-04-25 17:20:37 +0200

    Bouton clear all + addevent

--- script.js
+++ script.js
@@ -36,7 +36,6 @@ function board(size) {
 }
 
 
-
 function color(pixel) {
     pixel.addEventListener("mousedown", crayon);
     pixel.addEventListener("mouseover", event => {
@@ -46,17 +45,27 @@ function color(pixel) {
 
 
 
-let shade ='red';
+let shade = 'red';
+
+
 
 function crayon(event) {
+
     event.target.style.backgroundColor = shade;
 }
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-    shade = button.id;   
+        shade = button.id;
     });
 });
 
-
+const clear = document.querySelector('#clear')
+clear.addEventListener('click', () => {
+    const pixels = document.querySelectorAll('.pix')
+    pixels.forEach(pixel => {
+        pixel.style.backgroundColor = 'white'
+        shade = 'red'
+    });
+})
