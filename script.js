e20b479e9d4718fb1c44afb4ece983636ddd0e3c
Prasiinus <valentind9@hotmail.fr>
2023-04-25 18:48:12 +0200

    Bouton telecharger le dessin

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ range.addEventListener("click", function (e) {
     let size = validsize[this.value];
     inputvalue.textContent = `${size}x${size}`;;
     board(size);
+    clean();
 });
 
 
 function board(size) {
-
+    
     while (Container.firstChild) {
         Container.removeChild(Container.firstChild);
     }
@@ -32,7 +33,6 @@ function board(size) {
             color(pixel);
         }
     }
-
 }
 
 
@@ -45,27 +45,62 @@ function color(pixel) {
 
 
 
-let shade = 'red';
+let shade;
 
-
-
-function crayon(event) {
-
+function crayon(event) { 
     event.target.style.backgroundColor = shade;
 }
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
+        clean();
         shade = button.id;
     });
 });
 
 const clear = document.querySelector('#clear')
-clear.addEventListener('click', () => {
+clear.addEventListener('click', clean)
+
+function clean() 
+{
     const pixels = document.querySelectorAll('.pix')
     pixels.forEach(pixel => {
-        pixel.style.backgroundColor = 'white'
-        shade = 'red'
+        pixel.style.backgroundColor = 'white'});
+        shade = 'red';
+}
+
+const downloadBtn = document.querySelector('#download');
+downloadBtn.addEventListener('click', () => {
+    const canvas = document.createElement('canvas');
+    const container = document.querySelector('.container');
+    const containerRect = container.getBoundingClientRect();
+    const pixelSize = containerRect.width / container.children.length;
+
+    canvas.width = containerRect.width;
+    canvas.height = containerRect.height;
+
+    const ctx = canvas.getContext('2d');
+
+    Array.from(container.children).forEach((row, rowIndex) => {
+        Array.from(row.children).forEach((pixel, colIndex) => {
+            const { backgroundColor } = pixel.style;
+            const x = colIndex * pixelSize;
+            const y = rowIndex * pixelSize;
+
+            ctx.fillStyle = backgroundColor;
+            ctx.fillRect(x, y, pixelSize, pixelSize);
+        });
     });
-})
+
+    canvas.toBlob((blob) => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'mon_dessin.png';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    });
+});
